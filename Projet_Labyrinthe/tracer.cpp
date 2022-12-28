2f4a23f70746de8f7263917ec6646bf369e2a585
Maxime LEBRETON <maxime.lebreton@etu.univ-poitiers.fr>
2022-12-29 00:40:04 +0100

    ajout de méthode dans tracer

--- Projet_Labyrinthe/tracer.cpp
+++ Projet_Labyrinthe/tracer.cpp
@@ -1,1 +1,14 @@
 #include "tracer.h"
+
+coo::tracer::tracer()
+{
+}
+
+coo::tracer::~tracer()
+{
+}
+
+void coo::tracer::addMove(const direction& d)
+{
+	this->moveHistory.push_back(d);
+}
--- Projet_Labyrinthe/tracer.h
+++ Projet_Labyrinthe/tracer.h
@@ -1,5 +1,27 @@
 #pragma once
-class tracer
-{
-};
+#include "direction.h"
+#include <vector>
 
+namespace coo {
+	class tracer
+	{
+	public:
+		tracer();
+		~tracer();
+
+		/**
+		 * Ajoute le dernier mouvement effectué par le 
+		 * joueur dans l'historique.
+		 * 
+		 * \param d La direction prise par le joueur
+		 */
+		void addMove(const direction& d);
+
+	private:
+		/**
+		 * Ce vecteur contient une liste de direction qui 
+		 * est l'historique des déplacements du joueur.
+		 */
+		std::vector<direction> moveHistory;
+	};
+}
