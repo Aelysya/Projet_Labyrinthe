00e123069b4ff5bc11bb9d73bc7620cd6d466e1b
Maxime LEBRETON <maxime.lebreton@etu.univ-poitiers.fr>
2022-12-31 17:55:00 +0100

    add: vérification du nom du fichier entré par l'utilisateur

--- Projet_Labyrinthe/Projet_Labyrinthe.cpp
+++ Projet_Labyrinthe/Projet_Labyrinthe.cpp
@@ -1,7 +1,5 @@
-// Projet_Labyrinthe.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include <fstream>
 #include "leftPlayer.h"
 #include "rightPlayer.h"
 #include "yoloplayer.h"
@@ -11,34 +9,22 @@ using namespace coo;
 
 int main()
 {
-	//1: Choix du fichier
-	//saisie par l'utilisateur du nom du fichier au format .txt
-	//traitement du std::string -> supprimer tout avant /, supprimer tout après . et remplacer par .txt
-	
-	//2: Traitement du fichier
-	//todo: vérifier que fichier n'est pas vide 
-	// + qu'il fait au moins 3x3 de large 
-	// (position du joueur de départ = fin)
-	//sinon -> step1
+	bool fileNameOk;
+	std::string fileName;
+	do {
+		std::cout << "Entrez le nom du fichier contenant le labyrinthe: " << std::endl;
+		std::getline(std::cin, fileName);
+		std::ifstream testOpen(fileName);
+		fileNameOk = testOpen.is_open();
 
-	//3: Résolution du labyrinthe
-	//solver s("lab3_3x3.txt", 2);
-	//4: Affichage du résultat
-	//s.solve();
-	
-	//Tests autres
-	//grid g("lab3_3x3.txt", 2);
-	//g.printMaze(1,1);
-	/*yoloPlayer p(g);
+		if (!fileNameOk) {
+			std::cout << "Fichier invalide !" << std::endl;
+		}
+	} while (!fileNameOk);
 
-	p.printMaze();
-	p.solve();
-	p.printMaze();
-	p.printHistory();*/
-
-	leftPlayer lp("lab3_3x3.txt");
-	rightPlayer rp("lab3_3x3.txt");
-	yoloPlayer yp("lab3_3x3.txt");
+	leftPlayer lp(fileName);
+	rightPlayer rp(fileName);
+	yoloPlayer yp(fileName);
 
 	lp.solve();
 	rp.solve();
