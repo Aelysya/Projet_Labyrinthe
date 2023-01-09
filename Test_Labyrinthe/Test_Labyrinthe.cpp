4a8e5b9d4b3d1323329c5df54187d99b37ba99f1
Karameme <inesfeugier@gmail.com>
2023-01-09 02:26:34 +0100

    Mise en place test isSeen

--- Test_Labyrinthe/Test_Labyrinthe.cpp
+++ Test_Labyrinthe/Test_Labyrinthe.cpp
@@ -2,7 +2,9 @@
 #include "CppUnitTest.h"
 #include "../Projet_Labyrinthe/grid.cpp"
 #include "../Projet_Labyrinthe/tracer.cpp"
+#include "../Projet_Labyrinthe/direction.h"
 
+using namespace utility;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace TestLabyrinthe
@@ -22,10 +24,48 @@ namespace TestLabyrinthe
 	public:
 		TEST_METHOD(getMoves)
 		{
-			utility::tracer t(7, 7);
+			tracer t(3, 3);
 			Assert::IsTrue(t.getMoves() == 0);
-			t.addMove(1, 1, utility::direction::RIGHT);
+			t.addMove(1, 1, RIGHT);
 			Assert::IsTrue(t.getMoves() == 1);
+			t.addMove(1, 1, RIGHT);
+			Assert::IsTrue(t.getMoves() == 2);
+		}
+
+		TEST_METHOD(checkBlocked)
+		{
+			tracer t(5, 5);
+			//Todo
+		}
+
+		TEST_METHOD(isSeen)
+		{
+			u:tracer t(5, 5);
+			Assert::IsTrue(t.isSeen(1, 1) ==  NONE);
+			Assert::IsTrue(t.isSeen(2, 1) == NONE);
+			Assert::IsTrue(t.isSeen(1, 2) == NONE);
+			t.addMove(1, 1, RIGHT);
+			Assert::IsTrue(t.isSeen(1, 1) == RIGHT);
+			Assert::IsTrue(t.isSeen(2, 1) == RIGHT);
+			Assert::IsTrue(t.isSeen(1, 2) == NONE);
+			t.addMove(1, 1, DOWN);
+			Assert::IsTrue(t.isSeen(1, 1) == DOWN);
+			Assert::IsTrue(t.isSeen(2, 1) == RIGHT);
+			Assert::IsTrue(t.isSeen(1, 2) == DOWN);
+			t.addMove(2, 1, LEFT);
+			Assert::IsTrue(t.isSeen(1, 1) == LEFT);
+			Assert::IsTrue(t.isSeen(2, 1) == LEFT);
+			Assert::IsTrue(t.isSeen(1, 2) == DOWN);
+			t.addMove(1, 2, UP);
+			Assert::IsTrue(t.isSeen(1, 1) == UP);
+			Assert::IsTrue(t.isSeen(2, 1) == LEFT);
+			Assert::IsTrue(t.isSeen(1, 2) == UP);
+		}
+
+		TEST_METHOD(isAccessible)
+		{
+			tracer t(3, 3);
+			//Todo
 		}
 	};
 }
